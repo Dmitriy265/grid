04d7c7eac8a5b94a174ec9e64df905e2dc040435
Dmitriy265 <63237845+Dmitriy265@users.noreply.github.com>
2020-04-15 13:19:47 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>ТАБЛИЦЫ</title>
+    <style>
+      [class^="col-"] {
+         min-height: 100px;
+         border: 6px solid black;
+         box-sizing: border-box; 
+      }
+    </style>
+  </head>
+  <body style="background-color:Pink">>
+     <div class="container px-lg-5">
+  <div class="row mx-lg-n5">
+    <div class="col py-3 px-lg-5 border bg-light">ТАБЛИЦА № 1</div>
+    <div class="container">
+      <div class="row bg-warning ">
+        <div class="col-4">1</div>
+        <div class="col-3">2</div>
+        <div class="col-5">3</div>
+      </div>
+      <div class="container px-lg-5">
+  <div class="row mx-lg-n5">
+    <div class="col py-3 px-lg-5 border bg-light">КОЛОНКА 1/0</div>
+    <div class="col py-3 px-lg-5 border bg-light">КОЛОНКА 2/0</div>
+  </div>
+</div>
+      <div class="container">
+  <div class="row justify-content-start">
+    <div class="col-4">
+      1/1
+    </div>
+    <div class="col-4">
+      2/1
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-4">
+      1/2
+    </div>
+    <div class="col-4">
+      2/2
+    </div>
+  </div>
+  <div class="row justify-content-end">
+    <div class="col-4">
+      1/3
+    </div>
+    <div class="col-4">
+      2/3
+    </div>
+  </div>
+    </div> 
+    <div class="container px-lg-10">
+  <div class="row mx-lg-n5">
+    <div class="col py-3 px-lg-5 border bg-light">ТАБЛИЦА № 2 (продолжение)</div>
+    
+  </div>
+</div>
+    <div class="container">
+      <div class="row bg-warning ">
+        <div class="col-sm">4</div>
+        <div class="col-md">5</div>
+        <div class="col-sm">6</div>
+      </div>
+    </div>
+    <div class="container px-lg-5">
+  <div class="row mx-lg-n5">
+    <div class="col py-3 px-lg-5 border bg-light">7/0</div>
+    <div class="col py-3 px-lg-5 border bg-light">8/0</div>
+  </div>
+</div>
+  <div class="row justify-content-around">
+    <div class="col-4">
+      7/1
+    </div>
+    <div class="col-4">
+      8/1
+    </div>
+  </div>
+  <div class="row justify-content-between">
+    <div class="col-4">
+      7/2
+    </div>
+    <div class="col-4">
+      8/2
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col align-self-start">
+      1 НАПРАВЛЕНИЕ
+    </div>
+    <div class="col align-self-center">
+      2 НАПРАВЛЕНИЕ
+    </div>
+    <div class="col align-self-end">
+      3 НАПРАВЛЕНИЕ
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row bg-warning">
+    <div class="col-md-4">.col-md-4</div>
+    <div class="col-md-4 ml-auto">.col-md-4 .ml-auto</div>
+  </div>
+  <div class="row">
+    <div class="col-md-3 ml-md-auto">.col-md-3 .ml-md-auto</div>
+    <div class="col-md-3 ml-md-auto">.col-md-3 .ml-md-auto</div>
+  </div>
+  <div class="row">
+    <div class="col-auto mr-auto">.col-auto .mr-auto</div>
+    <div class="col-auto">.col-auto</div>
+  </div>
+</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
